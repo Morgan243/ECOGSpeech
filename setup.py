755b8f7b99fe52efa6808e09fee0b49fa34a2743
Morgan Stuart <morgansstuart243@gmail.com>
2020-05-14 13:29:55 -0400

    init commit of essential files/dirs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='ECOGSpeech',
+      version='0.1',
+      description='Processing ECOG+Voice recordings',
+      author='Morgan Stuart & Srdjan Lesaja',
+      packages=['ecog_speech'],
+      requires=[
+            'BCI2kReader',
+            'numpy', 'pandas',
+            'sklearn',
+            'attrs'])
