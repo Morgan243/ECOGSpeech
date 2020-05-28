e9f0bb4ecb99d4d023f3e29d54f9798975c59c60
Morgan Stuart <morgansstuart243@gmail.com>
2020-05-27 23:54:43 -0400

    Basic voice segmentation and example
    Example added to existing notebook

--- ecog_speech/feature_processing.py
+++ ecog_speech/feature_processing.py
@@ -9,3 +9,22 @@ def peak_normalization(arr_s, output_type=int):
     :return:
     """
     return ((arr_s / arr_s.max()) * np.iinfo(arr_s.dtype).max).round().astype(output_type)
+
+
+def compute_speaking_mask(wav, power_win_size=1024, avg_power_thresh=50, avg_power_win_size=4096, rate=48000):
+    pwr_s = wav.abs().rolling(power_win_size).mean()
+    return (pwr_s > avg_power_thresh).rolling(avg_power_win_size).max() > 0
+
+
+def speech_region_index_from_speech_mask(spk_mask_s):
+    leading_edge_s = ((spk_mask_s.diff().replace(0, np.nan) + 1) / 2)
+    word_trail_label_s = leading_edge_s.fillna(0).cumsum()
+    return spk_mask_s * word_trail_label_s
+
+
+def compute_speech_index(wav, speaking_mask_f=compute_speaking_mask):
+    """
+    Provide a function that
+    """
+    spk_mask_s = speaking_mask_f(wav).astype(int)
+    return speech_region_index_from_speech_mask(spk_mask_s)
