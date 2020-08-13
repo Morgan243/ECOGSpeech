2867d5ce644fe63ef3e22ecb4824c847448fe8ac
Morgan Stuart <morgansstuart243@gmail.com>
2020-08-12 21:59:28 -0400

    Filter func useable with pd structures;neutral labels
    Trying out using negative label values for silence preceding a spoken
    word. So the label for the silence before the 1st word (labeled 1) is
    labeled -1, for 2nd word it is -2, and so on. To get all silence, fitler
    to below zero. This helps make sure data from silent regions is
    contigous. But this make cause more confusion than it solves

--- ecog_speech/feature_processing.py
+++ ecog_speech/feature_processing.py
@@ -11,7 +11,11 @@ def peak_normalization(arr_s, output_type=int):
     return ((arr_s / arr_s.max()) * np.iinfo(arr_s.dtype).max).round().astype(output_type)
 
 
-def compute_speaking_mask(wav, power_win_size=1024, avg_power_thresh=50, avg_power_win_size=4096, rate=48000):
+def compute_speaking_mask(wav, power_win_size=1024,
+                          avg_power_thresh=50, avg_power_win_size=4096):
+    """
+    Uses rolling power estimate to gate the input - 1 or True is passthrough
+    """
     pwr_s = wav.abs().rolling(power_win_size).mean()
     return (pwr_s > avg_power_thresh).rolling(avg_power_win_size).max() > 0
 
@@ -19,7 +23,16 @@ def compute_speaking_mask(wav, power_win_size=1024, avg_power_thresh=50, avg_pow
 def speech_region_index_from_speech_mask(spk_mask_s):
     leading_edge_s = ((spk_mask_s.diff().replace(0, np.nan) + 1) / 2)
     word_trail_label_s = leading_edge_s.fillna(0).cumsum()
-    return spk_mask_s * word_trail_label_s
+    word_index = spk_mask_s * word_trail_label_s
+
+    # Incrementing negatives during silence, 0 everywhere else
+    neg_neutral_cnt = -(word_trail_label_s - word_index - spk_mask_s) - 1
+
+    # combine to make incrementing negatives identify leading silence regions
+    # and incrementing positive labels for speech
+    r_ix = pd.Series(np.where(neg_neutral_cnt == 0, word_index, neg_neutral_cnt),
+                     index=word_index.index)
+    return r_ix
 
 
 def compute_speech_index(wav, speaking_mask_f=compute_speaking_mask):
@@ -28,3 +41,39 @@ def compute_speech_index(wav, speaking_mask_f=compute_speaking_mask):
     """
     spk_mask_s = speaking_mask_f(wav).astype(int)
     return speech_region_index_from_speech_mask(spk_mask_s)
+
+
+######
+def filter(s, band, sfreq=1000, verbose=False, n_jobs=4,
+           method='fir'):
+    try:
+        import mne
+    except ImportError:
+        print("Need MNE package to filter")
+        raise
+    if isinstance(s, pd.Series):
+        _s = s.values
+    elif isinstance(s, pd.DataFrame):
+        _s = s.values.T
+    else:
+        _s = s
+
+    _s = _s.astype('float64')
+
+    filtered_phase_arr = mne.filter.filter_data(_s, sfreq,
+                                                *band,
+                                                verbose=verbose,
+                                                method=method,
+                                                n_jobs=n_jobs)
+
+    if isinstance(s, pd.Series):
+        ret = pd.Series(filtered_phase_arr, index=s.index, name=s.name)
+
+    elif isinstance(s, pd.DataFrame):
+        ret = pd.DataFrame(filtered_phase_arr.T,
+                           columns=s.columns,
+                           index=s.index)
+    else:
+        ret = filtered_phase_arr
+
+    return ret
