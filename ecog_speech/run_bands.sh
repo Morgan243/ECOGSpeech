17bf1adb3f3105f0a3b55b667bd7cd2d12ff833a
Morgan Stuart <morgansstuart243@gmail.com>
2021-03-10 20:42:02 -0500

    init script to experiment across bands

---
+++ ecog_speech/run_bands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#NUM_BANDS=(1 2)
+NUM_BANDS=(1 2 3 4 5)
+#NUM_BANDS=(40 50 60 80 90)
+LEARNING_RATES=(0.001)
+N_EPOCHS=15
+
+for num_bands in "${NUM_BANDS[@]}"
+do
+  for lr in "${LEARNING_RATES[@]}"
+  do
+    echo --num-bands=$num_bands --learning-rate=$lr "$@"
+    python experiments.py --result-dir='../results' \
+                          --dataset='nww' \
+                          --model-name='base-sn' \
+                          --sn-n-bands=$num_bands \
+                          --n-epochs=$N_EPOCHS \
+                          --learning-rate=$lr "$@"
+  done
+done
