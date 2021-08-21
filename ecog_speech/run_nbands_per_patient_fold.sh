0f1f691f7a0554c4d66c73868dca1b8513bf2b3c
Morgan Stuart <morgansstuart243@gmail.com>
2021-08-20 23:24:21 -0400

    Script to sweep only SN bands per patient

---
+++ ecog_speech/run_nbands_per_patient_fold.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUM_BANDS=(1 2 3)
+#N_FILTERS=(16 32)
+n_filters=16
+
+for num_bands in "${NUM_BANDS[@]}"
+do
+    ./run_per_patient_fold.sh "$@" --track-sinc-params --sn-n-bands=$num_bands --n-cnn-filters=$n_filters
+done
