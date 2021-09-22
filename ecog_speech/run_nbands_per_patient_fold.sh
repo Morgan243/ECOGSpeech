5266047e76390c61e2f1365906eef93e15f1a44f
Morgan Stuart <morgansstuart243@gmail.com>
2021-09-21 21:10:19 -0400

    Don't include n-cnn-filters in case it's being passed through

--- ecog_speech/run_nbands_per_patient_fold.sh
+++ ecog_speech/run_nbands_per_patient_fold.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 NUM_BANDS=(1 3 5)
 #N_FILTERS=(16 32)
-n_filters=16
+#n_filters=16
 
 for num_bands in "${NUM_BANDS[@]}"
 do
-    ./run_per_patient_fold.sh "$@" --track-sinc-params --sn-n-bands=$num_bands --n-cnn-filters=$n_filters
+    ./run_per_patient_fold.sh "$@" --track-sinc-params --sn-n-bands=$num_bands #--n-cnn-filters=$n_filters
 done
