c9f39ce88d7ad3a1f2e4a5218241547de74120c4
Morgan Stuart <morgansstuart243@gmail.com>
2022-05-24 22:19:51 -0400

    Define some experiment opts and use them

--- ecog_speech/experiments/semi_supervised.py
+++ ecog_speech/experiments/semi_supervised.py
@@ -6,28 +6,120 @@ from dataclasses import dataclass, field
 import json
 from os.path import join as pjoin
 from ecog_speech.experiments import base as bxp
+from ecog_speech.models import base as bmp
 from ecog_speech import datasets
-
+from simple_parsing import ArgumentParser, choice, subgroups
+from ecog_speech.models import base_transformers
+import torch
 
 logger = utils.get_logger('semi_supervised')
 
+@dataclass
+class SemisupervisedCodebookTaskOptions(bxp.TaskOptions):
+    task_name: str = "semisupervised_codebook_training"
+    dataset: datasets.DatasetOptions = datasets.DatasetOptions('hvs', train_sets='UCSD-19')
+    ppl_weight: float = 100.
 
-def make_datasets_and_loaders(options, **kwargs):
-    add_trfs = [datasets.SelectFromDim(dim=0, index='random', keep_dim=True)] if options.random_sensors_to_samples else None
-    data_kws = {k: dict(flatten_sensors_to_samples=options.flatten_sensors_to_samples,
-                        #additional_train_transforms=add_trfs, additional_eval_transforms=add_trfs
-                )
-                 for k in ['train_data_kws', 'cv_data_kws', 'test_data_kws']}
-    dataset_map, dl_map, eval_dl_map = standard.make_datasets_and_loaders(options, #dataset_cls=datasets.HarvardSentences,
-                                                                          pre_processing_pipeline=options.pre_processing_pipeline, #'audio_gate_speaking_only',
-                                                                          additional_transforms=add_trfs,
-                                                                          num_dl_workers=options.n_dl_workers,
-                                                                          #base_data_kws=dict(extra_output_keys=["sensor_ras_coord_arr"]),
-                                                                          base_data_kws=dict(extra_output_keys=options.extra_output_keys.split(',')
-                                                                                             if options.extra_output_keys is not None else None),
-                                                                          **data_kws, **kwargs)
+@dataclass
+class SemiSupervisedExperiment(bxp.Experiment):
+    model: bmp.ModelOptions = subgroups(
+        {
+            'cog2vec': base_transformers.Cog2VecOptions
+        }, default=base_transformers.Cog2VecOptions()
+    )
 
-    return dataset_map, dl_map, eval_dl_map
+    #task: bxp.TaskOptions = bxp.TaskOptions('semisupervised_training',
+    #                                        dataset=datasets.DatasetOptions('hvs', train_sets='UCSD-19'))
+    task: SemisupervisedCodebookTaskOptions = SemisupervisedCodebookTaskOptions()
+
+#def make_datasets_and_loaders(options, **kwargs):
+#    add_trfs = [datasets.SelectFromDim(dim=0, index='random', keep_dim=True)] if options.random_sensors_to_samples else None
+#    data_kws = {k: dict(flatten_sensors_to_samples=options.flatten_sensors_to_samples,
+#                        #additional_train_transforms=add_trfs, additional_eval_transforms=add_trfs
+#                )
+#                 for k in ['train_data_kws', 'cv_data_kws', 'test_data_kws']}
+#    dataset_map, dl_map, eval_dl_map = standard.make_datasets_and_loaders(options, #dataset_cls=datasets.HarvardSentences,
+#                                                                          pre_processing_pipeline=options.pre_processing_pipeline, #'audio_gate_speaking_only',
+#                                                                          additional_transforms=add_trfs,
+#                                                                          num_dl_workers=options.n_dl_workers,
+#                                                                          #base_data_kws=dict(extra_output_keys=["sensor_ras_coord_arr"]),
+#                                                                          base_data_kws=dict(extra_output_keys=options.extra_output_keys.split(',')
+#                                                                                             if options.extra_output_keys is not None else None),
+#                                                                          **data_kws, **kwargs)
+#
+#    return dataset_map, dl_map, eval_dl_map
+
+    def run(self):
+        dataset_map, dl_map, eval_dl_map = self.task.dataset.make_datasets_and_loaders()
+        model, model_kws = self.model.make_model(dataset_map['train'])
+        # Shake out any forward pass errors now by running example data through model
+        with torch.no_grad():
+            # model(model.t_x)
+            model(model.t_in)
+
+        # Default lr reduce to False, only setup if at patience is set
+        trainer_kws = dict(lr_adjust_on_cv_loss=False)
+        if self.task.lr_adjust_patience is not None:
+            print("Configuring LR scheduler for model")
+            lr_schedule_kws = dict(patience=self.task.lr_adjust_patience, factor=self.task.lr_adjust_factor,
+                                   verbose=True)
+            trainer_kws.update(dict(lr_adjust_on_plateau_kws=lr_schedule_kws,
+                                    lr_adjust_on_cv_loss=True,
+                                    model_name_to_lr_adjust='model'))
+
+        trainer = base_transformers.Cog2VecTrainer(model_map=dict(model=model), opt_map=dict(),
+                                                   train_data_gen=dl_map['train'], cv_data_gen=eval_dl_map['cv'],
+                                                   learning_rate=self.task.learning_rate,
+                                                   early_stopping_patience=self.task.early_stopping_patience,
+                                                   device=self.task.device,
+                                                   **trainer_kws)
+
+        # For some reason the codebook indices isn't always on the right device... so this seems to help force it over
+        #trainer.model_map['model'].quantizer.codebook_indices = trainer.model_map['model'].quantizer.codebook_indices.to(trainer.device)
+
+        trainer.squeeze_first = False
+        trainer.ppl_weight = self.task.ppl_weight
+
+        losses = trainer.train(self.task.n_epochs)
+
+        model.load_state_dict(trainer.get_best_state())
+
+        #####
+        # Produce predictions and score them
+        model.eval()
+        # outputs_map = trainer.generate_outputs(**eval_dl_map)
+        eval_res_map = {k: trainer.eval_on(_dl).to_dict(orient='list') for k, _dl in eval_dl_map.items()}
+
+        res_dict = self.create_result_dictionary(
+            batch_losses=losses,
+            train_selected_columns=dataset_map['train'].selected_columns,  # dataset_map['train'].selected_columns,
+            best_model_epoch=trainer.best_model_epoch,
+            num_trainable_params=utils.number_of_model_params(model),
+            num_params=utils.number_of_model_params(model, trainable_only=False),
+            model_kws=model_kws,
+            **eval_res_map,
+            **vars(self))
+
+        uid = res_dict['uid']
+        name = res_dict['name']
+
+        if self.result_output.save_model_path is not None:
+            import os
+            p = self.result_output.save_model_path
+            if os.path.isdir(p):
+                p = os.path.join(p, uid + '.torch')
+            logger.info("Saving model to " + p)
+            torch.save(model.cpu().state_dict(), p)
+            res_dict['save_model_path'] = p
+
+        if self.result_output.result_dir is not None:
+            path = pjoin(self.result_output.result_dir, name)
+            logger.info(path)
+            res_dict['path'] = path
+            with open(path, 'w') as f:
+                json.dump(res_dict, f)
+
+        return trainer, eval_res_map
 
 
 def run(options):
@@ -58,7 +150,6 @@ def run(options):
     with torch.no_grad():
         #model(model.t_x)
         model(model.t_in)
-
 
 
     # Default lr reduce to False, only setup if at patience is set
@@ -150,41 +241,46 @@ def run(options):
     #return trainer, outputs_map
     return trainer, eval_res_map
 
-@dataclass
-class SemiSupervisedOptions(bxp.DNNModelOptions, bxp.MultiSensorOptions):
-    # ###
-    model_name: str = 'cog2vec'      # Supported: {'cog2vec'}
-    dataset: str = 'hvs'
-    train_sets: str = 'UCSD-28'
-    pre_processing_pipeline: str = "audio_gate_speaking_only"
-    # ###
+#@dataclass
+#class SemiSupervisedOptions__OLD(bmp.DNNModelOptions, bxp.MultiSensorOptions):
+#    # ###
+#    model_name: str = 'cog2vec'      # Supported: {'cog2vec'}
+#    dataset: str = 'hvs'
+#    train_sets: str = 'UCSD-28'
+#    pre_processing_pipeline: str = "audio_gate_speaking_only"
+#    # ###
+#
+#    feature_extractor_layers: str = '[(128, 7, 3)] + [(128, 3, 2)] * 2 + [(256, 3, 1)]'
+#    """String that evaluates to list of tuples describing 1-d convolution feature extractor
+#        [(n-channels, kernel size, step size)..]"""
+#
+#    quant_num_vars: int = 20
+#    """Number of variables in quantizer codebook"""
+#    quant_num_groups: int = 2
+#    """Number of groups in quantizer"""
+#    ppl_weight: float = 100
+#    """Weight of perplexity loss - use to encourage full use of codebook"""
+#
+#    n_encoder_layers: int = 12
+#    n_encoder_heads: int = 8
 
-    feature_extractor_layers: str = '[(128, 7, 3)] + [(128, 3, 2)] * 2 + [(256, 3, 1)]'
-    """String that evaluates to list of tuples describing 1-d convolution feature extractor 
-        [(n-channels, kernel size, step size)..]"""
 
-    quant_num_vars: int = 20
-    """Number of variables in quantizer codebook"""
-    quant_num_groups: int = 2
-    """Number of groups in quantizer"""
-    ppl_weight: float = 100
-    """Weight of perplexity loss - use to encourage full use of codebook"""
-
-    n_encoder_layers: int = 12
-    n_encoder_heads: int = 8
-
-
-all_model_hyperparam_names = [k for k, v in SemiSupervisedOptions.__annotations__.items()
-                              if k not in ('train_sets', 'cv_sets', 'test_sets')]
+#all_model_hyperparam_names = [k for k, v in SemiSupervisedOptions.__annotations__.items()
+#                              if k not in ('train_sets', 'cv_sets', 'test_sets')]
 
 if __name__ == """__main__""":
     from simple_parsing import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_arguments(SemiSupervisedOptions, dest='semi_supervised')
+    parser.add_arguments(SemiSupervisedExperiment, dest='semi_supervised')
     args = parser.parse_args()
-    main_options: SemiSupervisedOptions = args.semi_supervised
+    experiment: SemiSupervisedExperiment = args.semi_supervised
+    experiment.run()
+
+    #parser.add_arguments(SemiSupervisedOptions, dest='semi_supervised')
+    #args = parser.parse_args()
+    #main_options: SemiSupervisedOptions = args.semi_supervised
     #main_options.random_sensors_to_samples = True
     #main_options.batches_per_epoch = 10
     #main_options.n_epochs = 1
-    run(main_options)
+    #run(main_options)
