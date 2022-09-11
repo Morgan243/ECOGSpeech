8e62bc5d14f72b29c86b17dec90f3c4496db4bcd
Morgan Stuart <morgansstuart243@gmail.com>
2022-09-11 16:57:15 -0400

    track conf matrix; plotting/inspect/ml off experiment;

--- ecog_speech/experiments/transfer_learning.py
+++ ecog_speech/experiments/transfer_learning.py
@@ -5,12 +5,15 @@ import time
 from datetime import datetime
 from os.path import join as pjoin
 
+import matplotlib.pyplot
+import sklearn.linear_model
 import torch
 from ecog_speech import utils
 from ecog_speech.models import base
 from typing import List, Optional
 from tqdm.auto import tqdm
 import numpy as np
+import pandas as pd
 from typing import ClassVar
 
 import attr
@@ -19,6 +22,7 @@ from ecog_speech.models.sinc_ieeg import make_model
 from ecog_speech.experiments import base as bxp
 from ecog_speech.models import base_fine_tuners as base_ft
 from ecog_speech import datasets
+from ecog_speech import visuals as viz
 from dataclasses import dataclass
 import json
 from simple_parsing import subgroups
@@ -119,7 +123,6 @@ class FineTuningModel(bmp.DNNModelOptions):
 
     classifier_head: Optional[Union[torch.nn.Module, str]] = None
 
-
     def create_fine_tuning_model(self, pretrained_model,
                                  n_pretrained_output_channels=None, n_pretrained_output_samples=None,
                                  #fine_tuning_method='1d_linear',
@@ -191,6 +194,10 @@ class FineTuningExperiment(bxp.Experiment):
          'word_detection': WordDetectionFineTuningTask()},
         default=RegionDetectionFineTuningTask())
     model: FineTuningModel = FineTuningModel()
+
+    inspection_plot_path: Optional[str] = None
+    n_rs_clf_iter: Optional[int] = None
+    rs_clf: Optional[str] = 'svm'
     # Don't need model options directly
     # TODO: make a fine tuning model options to capture at elast 'method' in task above
     #model: bmp.ModelOptions = subgroups(
@@ -265,64 +272,6 @@ class FineTuningExperiment(bxp.Experiment):
 
         return self.task.make_datasets_and_loaders(train_p_tuples=train_sets)
 
-#    @classmethod
-#    def create_fine_tuning_model(cls, pretrained_model,
-#                                 n_pretrained_output_channels=None, n_pretrained_output_samples=None,
-#                                 fine_tuning_method='1d_linear',
-#                                 dataset: datasets.BaseDataset = None,
-#                                 fine_tuning_target_shape=None, n_pretrained_input_channels=None,
-#                                 n_pretrained_input_samples=256,
-#                                 freeze_pretrained_weights=True,
-#                                 classifier_head: torch.nn.Module = None):
-#        from ecog_speech.models import base
-#        n_pretrained_output_channels = pretrained_model.C if n_pretrained_output_channels is None else n_pretrained_output_samples
-#        n_pretrained_output_samples = pretrained_model.T if n_pretrained_output_samples is None else n_pretrained_output_samples
-#
-#        n_pretrained_input_channels = n_pretrained_input_channels if dataset is None else len(dataset.selected_columns)
-#        # n_pretrained_input_samples = n_pretrained_input_samples if dataset is None else dataset.get_target_shape()
-#        fine_tuning_target_shape = dataset.get_target_shape() if fine_tuning_target_shape is None else fine_tuning_target_shape
-#
-#        m = copy.deepcopy(pretrained_model)
-#        m.quantizer.codebook_indices = None
-#        if freeze_pretrained_weights:
-#            for param in m.parameters():
-#                param.requires_grad = False
-#
-#        if fine_tuning_method == '1d_linear':
-#            # Very simple linear classifier head by default
-#            if classifier_head is None:
-#                h_size = 32
-#                classifier_head = torch.nn.Sequential(*[
-#                    base.Reshape((n_pretrained_output_channels * n_pretrained_output_samples,)),
-#                    torch.nn.Linear(n_pretrained_output_channels * n_pretrained_output_samples, h_size),
-#                    # torch.nn.BatchNorm1d(h_size),
-#                    torch.nn.LeakyReLU(),
-#                    torch.nn.Linear(h_size, h_size),
-#                    torch.nn.LeakyReLU(),
-#                    torch.nn.Linear(h_size, fine_tuning_target_shape),
-#                    # torch.nn.Sigmoid()
-#                ])
-#                # nn.init.xavier_uniform_(classifier_head.weight)
-#
-#            ft_model = base_ft.FineTuner(pre_trained_model=m, output_model=classifier_head,
-#                                         pre_trained_model_forward_kws=dict(features_only=True, mask=False),
-#                                         pre_trained_model_output_key='x',
-#                                         freeze_pre_train_weights=freeze_pretrained_weights)
-#
-#        elif '2d_' in fine_tuning_method :
-#            if dataset is None:
-#                raise ValueError(f"A dataset is required for '2d_*' methods in order to see num sensors")
-#            from ecog_speech.models import base_transformers
-#
-#            hidden_enc = 'transformer' if fine_tuning_method == '2d_transformers' else 'linear'
-#            ft_model = base_transformers.MultiChannelCog2Vec((n_pretrained_input_channels, n_pretrained_input_samples),
-#                                                             pretrained_model, outputs=dataset.get_target_shape(),
-#                                                             hidden_encoder=hidden_enc)
-#        else:
-#            raise ValueError(f"Unknown ft_method '{fine_tuning_method}'")
-#
-#        return ft_model
-
     def initialize(self):
         if getattr(self, 'initialized', False):
             return self
@@ -395,9 +344,28 @@ class FineTuningExperiment(bxp.Experiment):
     def eval(self):
         outputs_map = self.trainer.generate_outputs(**self.eval_dl_map)
 
+        class_val_to_label_d = next(iter(self.dataset_map['train'].data_maps.values()))['index_source_map']
+        class_labels = [class_val_to_label_d[i] for i in range(len(class_val_to_label_d))]
+        #out_d = self.outputs_map['train']
+        from sklearn.metrics import confusion_matrix
+        output_cm_map = dict()
+        for pname, out_d in outputs_map.items():
+            preds_arr: np.ndarray = out_d['preds'].squeeze()
+            if preds_arr.ndim == 1:
+                n_preds = 1 - preds_arr
+                preds_arr = np.concatenate([n_preds.reshape(-1, 1), preds_arr.reshape(-1, 1)], axis=1)
+            preds_df = pd.DataFrame(preds_arr, columns=class_labels)
+            y_s = pd.Series(out_d['actuals'].reshape(-1), index=preds_df.index, name='actuals')
+            y_label_s = y_s.map(class_val_to_label_d)
+            cm = confusion_matrix(y_label_s, preds_df.idxmax(1))
+            output_cm_map = pd.DataFrame(cm, columns=preds_df.columns, index=preds_df.columns).to_json()
+
+
         performance_map = dict()
+        clf_str_map = None
         if self.task.dataset.dataset_name == 'hvs':
             target_shape = self.dataset_map['train'].get_target_shape()
+
             #eval_res_map = {k: ft_trainer.eval_on(_dl).to_dict(orient='list') for k, _dl in eval_dl_map.items()}
             kws = dict(threshold=(0.5 if target_shape == 1 else None))
             clf_str_map = utils.make_classification_reports(outputs_map, **kws)
@@ -409,11 +377,17 @@ class FineTuningExperiment(bxp.Experiment):
                 performance_map = {part_name: utils.multiclass_performance(outputs_d['actuals'],
                                                                            outputs_d['preds'].argmax(1))
                                    for part_name, outputs_d in outputs_map.items()}
-        return performance_map
+            print(performance_map)
+
+        return outputs_map, output_cm_map, performance_map, clf_str_map
 
     def run(self):
+        if self.inspection_plot_path is not None:
+            self.make_pretrain_inspection_plots()
+            return
+
         self.train()
-        performance_map = self.eval()
+        self.outputs_map, self.output_cm_map, self.performance_map, self.clf_str_map = self.eval()
 
         #####
         # Prep a results structure for saving - everything must be json serializable (no array objects)
@@ -428,7 +402,8 @@ class FineTuningExperiment(bxp.Experiment):
             num_trainable_params=utils.number_of_model_params(self.fine_tune_model),
             num_params=utils.number_of_model_params(self.fine_tune_model, trainable_only=False),
             model_kws=self.fine_tune_model_kws,
-            **performance_map,
+            classification_summaries=self.clf_str_map,
+            **self.performance_map,
             #**eval_res_map,
             pretrained_result_input=vars(self.pretrained_result_input),
             task_options=vars(self.task),
@@ -437,7 +412,8 @@ class FineTuningExperiment(bxp.Experiment):
             #dataset_options=vars(self.dataset),
             result_output_options=vars(self.result_output),
             model_options=vars(self.model),
-            pretrained_results=self.pretraining_results
+            pretrained_results=self.pretraining_results,
+            confusion_matrices=self.output_cm_map
         )
         uid = res_dict['uid']
         name = res_dict['name']
@@ -445,8 +421,193 @@ class FineTuningExperiment(bxp.Experiment):
         self.save_results(self.fine_tune_model, result_file_name=name, result_output=self.result_output,
                           model_file_name=uid, res_dict=res_dict)
 
-        return self.trainer, performance_map
+        return self.trainer, self.performance_map
 
+    def make_pretrain_inspection_plots(self):
+        self.initialize()
+        p = self.inspection_plot_path
+        if p == 'AUTO':
+            p = f"pretrain_inspect_{tl.task.task_name}_{tl.pretraining_results['uid']}.pdf"
+
+        fig_d = self.make_plots()
+        utils.figures_to_pdf(p, **fig_d)
+
+    def make_plots(self):
+
+        t_dataset = self.dataset_map['train']
+        class_val_to_label_d = next(iter(t_dataset.data_maps.values()))['index_source_map']
+        print(class_val_to_label_d)
+        device = 'cuda'
+
+        from tqdm.auto import tqdm
+        import torch
+
+        fig_d = dict()
+        fig, axs = matplotlib.pyplot.subplots(ncols=2)
+        loss_df = pd.DataFrame(self.pretraining_results['epoch_outputs']).T
+        title = "".join([(" " + word if ix % 7 else "\n" + word) for ix, word in enumerate(str(self).split())])
+        ax = loss_df[['total_loss', 'cv_loss', 'accuracy']].plot(secondary_y='accuracy', logy=True,
+                                                                 #title=f"Training of {self.task.task_name}",
+                                                                 ax=axs[0])
+        #ax.set_title(title, fontsize=8)
+        fig.text(1., 1., title, fontsize=8)
+        fig.suptitle(f"Training of {self.task.task_name}", y=1.)
+
+        results_l = list()
+        m = self.fine_tune_model.mc_from_1d.to(device).eval()
+        _dl = self.dl_map['train']
+        for batch_d in tqdm(_dl, desc="Running on train batches"):
+            with torch.no_grad():
+                dev_batch_d = {k: arr.to(device) for k, arr in batch_d.items()}
+                feat_d = m.forward(dev_batch_d)  # , features_only=True, mask=False)
+                results_l.append(
+                    dict(signal_arr=batch_d['signal_arr'],
+                         target_arr=batch_d['target_arr'],
+                         **{n: arr.detach().cpu().numpy() for n, arr in feat_d.items()}))
+
+        shape_str = str({k: a.shape for k, a in results_l[0].items()})
+        print(f"SHAPES: \n {shape_str}")
+        # all_x = np.concatenate([r['x'] for r in results_l])
+        all_x = np.concatenate([r['output'] for r in results_l])
+        all_x_df = pd.DataFrame(all_x.reshape(all_x.shape[0], -1))
+
+        # If there was a target
+        all_y = np.concatenate([r['target_arr'] for r in results_l])
+        all_y_s = pd.Series(all_y.squeeze(), name='target_val')
+        all_y_label_s = all_y_s.map(class_val_to_label_d).rename('target_label')
+
+        corr_speak_df = all_x_df.corrwith(all_y_s)
+
+        ax = corr_speak_df.hist(ax=axs[1])
+        #ax.text(0.01, 0.5, "Hist. of flattened b2v features corrwith target ")
+        ax.set_title("Hist. of flattened b2v features corrwith target ")
+        #kws_str = "".join((" " if i % 5 else "\n") + (f"{str(k)} = {str(v)}") for i, (k, v) in self.pretraining_results['model_kws'].items())
+        kws_str = "".join((" " + s if i % 5 else "\n" + s)
+                          for i, s in enumerate(str(self.pretraining_results['model_kws']).split()))
+        #kws_str = str(self.pretraining_results['model_kws'])
+        fig.text(1., 0.5, kws_str, fontsize=7)
+        fig.text(1., 0.1, "Batch data shapes as multichannel pretrained:\n" + shape_str, fontsize=7)
+
+        fig.tight_layout()
+        #ax.set_title(kws_str, fontsize=10)
+        fig_d['pretrain_subplots'] = fig
+
+
+        _pca_df, fig, axs = viz.pca_and_scatter_plot(all_x_df, c=all_y_s, cmap='jet', alpha=0.3)
+        fig_d['pca_2d_scatter'] = fig
+        _pca_df, fig, axs = viz.pca_and_pair_plot(all_x_df, all_y_label_s,
+                                                  n_components=4)
+        fig_d['pca_2d_pair'] = fig
+        s_df = _pca_df.sample(1000)
+        fig, ax = viz.scatter3d(*[s_df.iloc[:, i] for i in range(3)], c=all_y_s.loc[s_df.index])
+        fig_d['pca_3d_scatter'] = fig
+
+        from sklearn.manifold import TSNE
+        n_tsne = 3
+        tsne = TSNE(n_components=n_tsne, verbose=3, n_jobs=4, n_iter=3000, learning_rate=100, perplexity=50)
+        # tsne_pca_arr = tsne.fit_transform(_pca_df)
+        print(all_x_df.head())
+        print(all_x_df.dtypes)
+        print(all_x_df.dtypes.value_counts())
+
+        tsne_pca_arr = tsne.fit_transform(all_x_df)
+        tsne_pca_df = pd.DataFrame(tsne_pca_arr, columns=[f"TSNE-{n}" for n in range(n_tsne)])
+
+        all_y_label_s = all_y_s.map(class_val_to_label_d).rename('target_label')
+
+        import seaborn as sns
+        _plt_df = tsne_pca_df.join(all_y_label_s)
+        # _plt_df = _plt_df.assign(target_label=_plt_df.target_val.map(class_val_to_label_d))
+        plt_kws = dict(hue='target_label', diag_kws=dict(common_norm=False), palette='Set1', kind='hist',
+                       diag_kind='kde', vars=_plt_df.columns.drop(['target_label']).tolist())
+        g = sns.pairplot(_plt_df, **plt_kws)
+        fig_d['tsne_pair'] = g.fig
+
+        ###
+        from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
+        from sklearn.cluster import KMeans
+        from sklearn.svm import SVC
+        from sklearn.linear_model import LogisticRegression, SGDClassifier
+        from sklearn.model_selection import cross_validate, GridSearchCV, RandomizedSearchCV
+        from sklearn.metrics import f1_score, make_scorer, classification_report
+        from sklearn.pipeline import Pipeline
+
+        from tempfile import mkdtemp
+        from shutil import rmtree
+
+        # from sklearn.externals.joblib import Memory
+        from joblib import Memory
+        from sklearn.pipeline import Pipeline
+        from sklearn.decomposition import PCA
+        from sklearn.svm import LinearSVC
+
+        #import autosklearn.classification
+        #import autosklearn
+        #automl = autosklearn.classification.AutoSklearnClassifier(
+        #    time_left_for_this_task=120,
+        #    per_run_time_limit=30,
+        #    tmp_folder=f'/tmp/{self.task.task_name}_{self.pretraining_results["uid"]}',
+        #)
+        #automl.fit(all_x_df, all_y_s, dataset_name='breast_cancer')
+        #print(automl.leaderboard())
+        if self.n_rs_clf_iter is not None:
+            #clf = 'hist'
+            max_components = min(all_x_df.shape) // 8
+            if self.rs_clf == 'svm':
+                # the pipeline
+                pipe = Pipeline([('reduce_dim', PCA()),
+                                 ('clf', SVC(kernel='rbf'))])
+                param_dist = dict(  # clf__max_leaf_nodes=range(20, 200),
+                                           # clf__max_bins=range(4, 64),
+                                           # clf__learning_rate=np.arange(0.0001, 3, 0.001)
+                                           clf__C=np.arange(0.0001, 500, 0.001),
+                                           # clf__max_features=range(5, 500, 10),
+                                           reduce_dim__n_components=range(2, max_components)
+                                       )
+            elif self.rs_clf == 'hist':
+                pipe = Pipeline([('reduce_dim', PCA()),
+                                 ('clf', HistGradientBoostingClassifier())])
+                param_dist = dict(
+                    clf__max_leaf_nodes=range(10, 100),
+                    clf__max_bins=range(4, 64),
+                    clf__learning_rate=np.arange(0.0001, 2, 0.0001),
+                    # clf__max_features=range(5, 500, 10),
+                    reduce_dim__n_components=range(2, max_components)
+                )
+            elif self.rs_clf == 'sgd':
+                pipe = Pipeline([
+                    ('clf', sklearn.linear_model.SGDClassifier()) ])
+                param_dist = dict(
+                    clf__penalty=['l2', 'l1', 'elasticnet'],
+                    clf__loss=['modified_huber', 'hinge'],
+                    clf__alpha=np.arange(0.0001, 0.5, 0.0001)
+                )
+
+            rs = RandomizedSearchCV(pipe,
+                                       param_distributions=param_dist,
+                                       return_train_score=True,
+                                       n_iter=self.n_rs_clf_iter, n_jobs=8, cv=3, verbose=2)
+            rs.fit(all_x_df, all_y_s)
+            print(rs.best_score_)
+
+            rs_res_df = pd.DataFrame(rs.cv_results_).set_index('rank_test_score').sort_index()
+
+            import seaborn as sns
+            try:
+                g = sns.pairplot(rs_res_df.reset_index(), vars=rs_res_df.filter(like='param_').columns.tolist(),
+                             hue='mean_test_score')
+                fig_d['rs_svm_pairplot'] = g.fig
+                g.fig.suptitle(str(pipe))
+                g.fig.tight_layout()
+            except Exception as e:
+                print(str(e))
+                grp_k = rs_res_df.filter(like='param_').columns.tolist()
+                ax = rs_res_df.groupby(grp_k).mean_test_score.mean().sort_values().plot.barh(figsize=(7, 15))
+                fig = ax.get_figure()
+                fig.tight_layout()
+                fig_d['rs_barplot'] = fig
+
+        return fig_d
 
 
 @attr.s
@@ -576,6 +737,7 @@ if __name__ == """__main__""":
     from simple_parsing import ArgumentParser
 
     parser = ArgumentParser(description="ASPEN+MHRG Transfer Learning experiments")
+    #parser.add_arguments("--pretrained_inspection_plots", action='store_true', default=False)
     parser.add_arguments(FineTuningExperiment, dest='transfer_learning')
     #parser.add_arguments(TransferLearningOptions, dest='transfer_learning')
     #parser.add_arguments(TransferLearningResultParsingOptions, dest='tl_result_parsing')
